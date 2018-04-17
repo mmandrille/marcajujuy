9ee9e2714b2e3b53899a6bdfa772b3d7e51a2b11
servidor <servidor@DjangoGob.DjangoGob>
2018-04-17 17:06:10 -0300

    Se realizaron cambios.

--- gunicorn.sh
+++ gunicorn.sh
@@ -1,3 +1,6 @@
-#!/bin/bash
+!/bin/bash
 cd /opt/marcajujuy
-gunicorn MarcaJujuy.wsgi -t 600 -b 127.0.0.1:8000 -w 6 --user=root --group=root --/opt/marcajujuy/gunicorn.log 2>>/opt/marcajujuy/gunicorn.log
+source venv/bin/activate
+cd /opt/marcajujuy/marcajujuy
+gunicorn MarcaJujuy.wsgi -t 600 -b 127.0.0.1:8000 -w 6 --user=servidor --group=servidor --log-file=/opt/marcajujuy/marcajujuy/gunicorn.log 2>>/opt/marcajujuy/marcajujuy/gunicorn.log
+
