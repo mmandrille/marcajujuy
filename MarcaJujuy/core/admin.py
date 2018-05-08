c014e2c0edb8391ce9c5d5e34b781d98ae8a8530
Marcos Federico Mandrille <mmandrille@gmail.com>
2018-05-08 13:55:47 -0300

    Mega limpieza

--- MarcaJujuy/core/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Archivo, Comentario, Faq, Consulta, Respuesta
-# Register your models here.
-admin.site.register(Archivo)
-admin.site.register(Comentario)
-admin.site.register(Faq)
-admin.site.register(Consulta)
-admin.site.register(Respuesta)
--- MarcaJujuy/gunicorn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd /opt/marcajujuy
-source venv/bin/activate
-cd /opt/marcajujuy/marcajujuy
-gunicorn MarcaJujuy.wsgi -t 600 -b 127.0.0.1:8000 -w 6 --user=servidor --group=servidor --log-file=/opt/marcajujuy/marcajujuy/gunicorn.log 2>>/opt/marcajujuy/marcajujuy/gunicorn.log
-
